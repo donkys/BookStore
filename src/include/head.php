6af052dc5aff9eb42f853b3e503f7759781259fa
donkys <63050156@kmitl.ac.th>
2023-02-17 16:22:29 +0700

    change ico path

--- src/include/head.php
+++ src/include/head.php
@@ -13,5 +13,5 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@200;400;500;600;700&family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,300&family=Kanit:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400&family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,800;1,400;1,500;1,600;1,800&family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="icon" href="../favicon.ico" />
+    <link rel="icon" href="./favicon.ico" />
     <link href="./dist/output.css" rel="stylesheet">
